bb61faa4d0b693f8e07b66ef8d6a1596be61b371
Donjeta Ramadani <donjetaramadani90@gmail.com>
2023-12-09 18:44:39 +0200

    Index-script Final

--- script.js
+++ script.js
@@ -1,31 +1,26 @@
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 
-
-const heroHeading = document.querySelector(".hero-heading");
-const heroText = document.querySelector(".hero-text");
-ScrollSmoother.create({
-  content: "#content",
-  wrapper: "#viewport",
-  smooth: 2
-})
-
+	const heroHeading = document.querySelector(".hero-heading");
+	const heroText = document.querySelector(".hero-text");
+	ScrollSmoother.create({
+	content: "#content",
+	wrapper: "#viewport",
+	smooth: 2
+	})
 
 
-let scrollEls = document.querySelector(".section-hero");
+	let scrollEls = document.querySelector(".section-hero");
 
-let scl = ScrollTrigger.create({
-  trigger: scrollEls,
-  pin: ".hero-bg-img",
-  start: "-140px top",
-  end: "bottom top",
-  markers: true,
-});
-
-
+	let scl = ScrollTrigger.create({
+		trigger: scrollEls,
+		pin: ".hero-bg-img",
+		start: "-140px top",
+		end: "bottom top",
+		markers: true,
+	});
 
 
-// this is the helper function that sets it all up.It also sets the default "scroller" to the content so you don't have to do that on all your ScrollTriggers.
 function smoothScroll(content, viewport, smoothness) {
 	content = gsap.utils.toArray(content)[0];
 	smoothness = smoothness || 1;
@@ -44,33 +39,34 @@ function smoothScroll(content, viewport, smoothness) {
 		},
 		height, isProxyScrolling;
 
-	function refreshHeight() {
-		height = content.clientHeight;
-		content.style.overflow = "visible"
-		document.body.style.height = height + "px";
-    return height - document.documentElement.clientHeight;
-	}
+		function refreshHeight() {
+			height = content.clientHeight;
+			content.style.overflow = "visible"
+			document.body.style.height = height + "px";
+		return height - document.documentElement.clientHeight;
+		}
 
-	ScrollTrigger.addEventListener("refresh", () => {
-		removeScroll();
-		requestAnimationFrame(removeScroll);
-	})
-	ScrollTrigger.defaults({scroller: content});
-	ScrollTrigger.prototype.update = p => p; 
+		ScrollTrigger.addEventListener("refresh", () => {
+			removeScroll();
+			requestAnimationFrame(removeScroll);
+		})
 
-	ScrollTrigger.scrollerProxy(content, {
-		scrollTop(value) {
-			if (arguments.length) {
-				isProxyScrolling = true; 
-				setProp(-value);
-				setScroll(value);
-				return;
-			}
-			return getProp("y");
+		ScrollTrigger.defaults({scroller: content});
+		ScrollTrigger.prototype.update = p => p;
+
+		ScrollTrigger.scrollerProxy(content, {
+			scrollTop(value) {
+				if (arguments.length) {
+					isProxyScrolling = true;
+					setScroll(value);
+					return;
+				}
+				return getProp("y");
 		},
-    scrollHeight: () => document.body.scrollHeight,
-		getBoundingClientRect() {
-			return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+
+		scrollHeight: () => document.body.scrollHeight,
+			getBoundingClientRect() {
+				return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
 		}
 	});
 
@@ -92,9 +88,10 @@ function smoothScroll(content, viewport, smoothness) {
 				isProxyScrolling = false;
 			}
 		},
-		onRefresh: killScrub // when the screen resizes, we just want the animation to immediately go to the appropriate spot rather than animating there, so basically kill the scrub.
+		onRefresh: killScrub 
 	});
 }
+
 
 const collapsibleButtons = document.querySelectorAll(
 	".collapsible-trigger-btn"
@@ -115,4 +112,4 @@ const collapsibleButtons = document.querySelectorAll(
 		e.currentTarget.nextElementSibling.style.height = 0;
 	  }
 	});
-  });
+});
